5bbbda099e68a4188328f101b65c977c1d527089
Minhajh2o <h2o.minhaj@gmail.com>
2025-06-09 23:31:35 +0600

    first commit. basic project created.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <div className="container max-w-7xl mx-auto p-4">
+        <h1>Welcome to My App</h1>
+        <p>This is a simple React application.</p>
+        <p>Feel free to explore and modify the code!</p>
+        <p>Happy coding!</p>
+        <p>Enjoy your day!</p>
+        <p>Keep learning and growing!</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
