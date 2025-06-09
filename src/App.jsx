283de39849792d74f3a381059e84ecae39ae1e18
Minhajh2o <h2o.minhaj@gmail.com>
2025-06-10 00:30:09 +0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import Header from "./components/header/Header";
+
 function App() {
   return (
     <>
-      <div className="container max-w-7xl mx-auto p-4">
-        <h1>Welcome to My App</h1>
-        <p>This is a simple React application.</p>
-        <p>Feel free to explore and modify the code!</p>
-        <p>Happy coding!</p>
-        <p>Enjoy your day!</p>
-        <p>Keep learning and growing!</p>
+      <div className="container max-w-7xl mx-auto p-2">
+        <Header />
+        <div className="p-2">
+          <h1>Welcome to My App</h1>
+          <p>This is a simple React application.</p>
+          <p>Feel free to explore and modify the code!</p>
+          <p>Happy coding!</p>
+          <p>Enjoy your day!</p>
+          <p>Keep learning and growing!</p>
+        </div>
       </div>
     </>
   );
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,58 @@
-import React from 'react';
+import React from "react";
+import { MdOutlineAccountCircle } from "react-icons/md";
+
 
 const Header = () => {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <div className="navbar justify-between bg-base-100 shadow-sm">
+        <div className="">
+          <a className="text-xl font-bold">Recipe Calories</a>
         </div>
-    );
+        <div className="flex gap-4 text-gray-700">
+            <a href="#">Home</a>
+            <a href="#">Recipes</a>
+            <a href="#">About</a>
+            <a href="#">Contact</a>
+        </div>
+        <div className="flex gap-2">
+          <input
+            type="text"
+            placeholder="Search"
+            className="input input-bordered w-24 md:w-auto"
+          />
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full bg-emerald-300">
+                <MdOutlineAccountCircle className="p-1 w-full h-full" />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
