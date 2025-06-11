356e971b16e427e00502509621ede7f5a2be413b
Minhajh2o <h2o.minhaj@gmail.com>
2025-06-11 18:27:00 +0600

    banner added

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -9,12 +9,6 @@ const Banner = () => {
         src={banner}
         alt="Banner Image"
       />
-      {/* <div className='absolute inset-0 bg-black opacity-30 flex flex-col gap-6 justify-center items-center text-center'>
-                <h1 className='text-6xl w-3/4 font-bold text-white'>Discover an exceptional cooking class tailored for you!</h1>
-                
-                <p className='text-lg text-white w-3/4'>Join our cooking class to enhance your culinary skills and impress your loved ones with delicious homemade meals. Unleash your inner chef today and create unforgettable memories in the kitchen!</p>
-                <div></div>
-            </div> */}
       <div className="absolute inset-0 flex flex-col gap-6 justify-center items-center text-center text-white">
         <div className="absolute inset-0 bg-black/35"></div>
         <h1 className="relative text-6xl w-3/4 font-bold">
